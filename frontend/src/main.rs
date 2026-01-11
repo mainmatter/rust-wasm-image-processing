084fe3b30d7133152ead87e468783282edd75241
Dax Huiberts <daxhuiberts@gmail.com>
2026-01-11 10:22:31 +0100

    Add foundation for contentful workshop

---
+++ frontend/src/main.rs
@@ -0,0 +1,17 @@
+use wasm_bindgen::prelude::*;
+
+fn main() {}
+
+#[wasm_bindgen]
+pub fn exercise_1(image_data: &[u8]) -> Vec<u8> {
+    let mut photon_image = photon::native::open_image_from_bytes(image_data).unwrap();
+    transformers::exercise_1::transform(&mut photon_image);
+    photon_image.get_bytes_jpeg(80)
+}
+
+#[wasm_bindgen]
+pub fn exercise_2(image_data: &[u8], do_one_thing: bool, value: f32) -> Vec<u8> {
+    let mut photon_image = photon::native::open_image_from_bytes(image_data).unwrap();
+    transformers::exercise_2::transform(&mut photon_image, do_one_thing, value);
+    photon_image.get_bytes_jpeg(80)
+}
