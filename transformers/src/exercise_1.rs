ce95e8da2aabe714102413db380753532f29e244
Jonas Kruckenberg <iterpre@protonmail.com>
2026-01-16 15:14:09 +0100

    exercise 01

--- transformers/src/exercise_1.rs
+++ transformers/src/exercise_1.rs
@@ -1,5 +1,12 @@
 use photon::PhotonImage;
 
+/// Welcome to the Wasm Image Transformation Workshop!
+///
+/// During our short time together you will see a bunch of cool things that Rust & WebAssembly can do!
+///
+/// This workshop is divided into four exercises, each one a bit more complicated than the next.
+/// During these exercises we will be building **image transformers** that accept an input image, apply
+/// some transformation and return an output image.
 pub fn transform(img: &mut PhotonImage) {
-    photon::filters::dramatic(img);
+    photon::transform::flipv(img);
 }
