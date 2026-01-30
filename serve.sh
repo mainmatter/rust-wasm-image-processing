cb42f859905aabbe6169a727542b5770f02adb0a
Magnus Markling <magnus@markling.com>
2026-01-30 11:17:22 +0100

    add --dev to wasm-pack command

--- serve.sh
+++ serve.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 watchexec --watch transformers --watch frontend/src -r 'wasm-pack build frontend --target no-modules --out-dir wasm --no-typescript && mdbook serve frontend' &
+watchexec --watch transformers --watch frontend/src -r 'wasm-pack build frontend --target no-modules --out-dir wasm --no-typescript --dev && mdbook serve frontend' &
 watchexec --watch transformers -r cargo run --bin backend
+
 wait
