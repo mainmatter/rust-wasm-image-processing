ec7548f1cb4362f5419f5d2201097eb10b3b08b7
Magnus Markling <magnus@markling.com>
2026-02-02 11:15:48 +0100

    update serve.sh

--- serve.sh
+++ serve.sh
@@ -25,8 +25,9 @@ if ! command -v watchexec >/dev/null 2>&1; then
   exit 1
 fi
 
-# We must use 0.0.0.0 as hostname for this to work in a dev container.
+# We must bind to 0.0.0.0 for this to work in a dev container.
 watchexec --watch exercises --watch frontend/src -r 'wasm-pack build frontend --target no-modules --out-dir wasm --no-typescript --no-pack --dev && mdbook serve --hostname 0.0.0.0 -p 3000 frontend' &
-watchexec --watch exercises --watch backend/src -r cargo run --bin backend
+
+watchexec --watch exercises --watch  backend/src -r 'cargo run --bin backend'
 
 wait
