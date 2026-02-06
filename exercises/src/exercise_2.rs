2908c4120de03597b3f0f2e0638050dabfc6cec8
Jonas Kruckenberg <iterpre@protonmail.com>
2026-02-06 11:04:23 +0100

    solutions

--- exercises/src/exercise_2.rs
+++ exercises/src/exercise_2.rs
@@ -1,7 +1,15 @@
 use photon::PhotonImage;
 
 /// A transformation that applies a color filter. The filter is determined by the `filter` argument.
-pub fn transform(_img: &mut PhotonImage, _filter: &str) {
-    // use the `match` statement to call the right transformation function depending on the argument.
-    todo!()
+pub fn transform(img: &mut PhotonImage, filter: &str) {
+    match filter {
+        "cali" => photon::filters::cali(img),
+        "dramatic" => photon::filters::dramatic(img),
+        "duotone_horizon" => photon::filters::duotone_horizon(img),
+        "duotone_lilac" => photon::filters::duotone_lilac(img),
+        "golden" => photon::filters::golden(img),
+        "lofi" => photon::filters::lofi(img),
+        "pastel_pink" => photon::filters::pastel_pink(img),
+        _ => panic!("no such filter"),
+    }
 }
