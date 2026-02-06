7db3abae1915c04cf7ad91ed55814f0fbd04dc24
Magnus Markling <magnus@markling.com>
2026-02-06 11:27:56 +0100

    fix compiler warnings

--- exercises/src/exercise_2.rs
+++ exercises/src/exercise_2.rs
@@ -1,7 +1,7 @@
 use photon::PhotonImage;
 
 /// A transformation that applies a color filter. The filter is determined by the `filter` argument.
-pub fn transform(img: &mut PhotonImage, filter: &str) {
+pub fn transform(_img: &mut PhotonImage, _filter: &str) {
     // use the `match` statement to call the right transformation function depending on the argument.
     todo!()
 }
--- exercises/src/exercise_4.rs
+++ exercises/src/exercise_4.rs
@@ -1,10 +1,11 @@
 use photon::PhotonImage;
+#[allow(unused)]
 use rayon::prelude::*;
 
-const BYTES_PER_PIXEL: u32 = 4; // red, green, blue, alpha
+const _BYTES_PER_PIXEL: u32 = 4; // red, green, blue, alpha
 
 /// Creates a thumbnail strip with a color filter applied, using parallel iteration.
-pub fn transform(img: &PhotonImage, widths: &[u32]) -> PhotonImage {
+pub fn transform(_img: &PhotonImage, _widths: &[u32]) -> PhotonImage {
     // Create a parallel iterator pipeline:
     // 1. Iterate over widths in parallel
     // 2. Map each width to a resized + filtered thumbnail
