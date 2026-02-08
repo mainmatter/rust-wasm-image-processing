fe17cd7456186f7c48fe2f6fb6258064f960425f
Magnus Markling <magnus@markling.com>
2026-02-08 10:10:08 +0100

    small fixes

--- exercises/src/exercise_3.rs
+++ exercises/src/exercise_3.rs
@@ -1,5 +1,6 @@
 use photon::PhotonImage;
 
+#[allow(unused)]
 const BYTES_PER_PIXEL: u32 = 4; // red, green, blue, alpha
 
 /// Stitches two images together horizontally.
@@ -7,12 +8,12 @@ pub fn transform(left: &PhotonImage, right: &PhotonImage) -> PhotonImage {
     assert!(left.get_width() > 0);
     assert!(right.get_width() > 0);
 
-    let new_width = left.get_width() + right.get_width();
-    let new_height = left.get_height().max(right.get_height());
+    let _new_width = left.get_width() + right.get_width();
+    let _new_height = left.get_height().max(right.get_height());
 
     // TODO initialize a vec that has space for new new_width * new_height pixels
-    // TODO Copy left image to the start.
-    // TODO Copy right image to where the left image ends.
+    // TODO copy left image to the start
+    // TODO copy right image to where the left image ends
 
     // PhotonImage::new(pixels, new_width, new_height)
     todo!()
